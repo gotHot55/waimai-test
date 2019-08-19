eecf2c46f2c58e08d5b6a3ef796b26990ff8ce3c
gotHot55 <595105385@qq.com>
2019-08-19 23:57:17 +0800

    商家滑动定位效果

--- src/store/actions.js
+++ src/store/actions.js
@@ -69,11 +69,13 @@ export default {
   },
 
   //异步获取商家商品列表
-  async getShopGoods({commit}){
+  async getShopGoods({commit},callback){
     const result=await reqShopGoods()
     if (result.code===0){
       const goods=result.data
       commit(RECEIVE_GOODS,{goods})
+      //加载完毕
+      callback && callback()
     }
   },
   //异步获取商家评价列表
